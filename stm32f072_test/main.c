b451faa317bb2d1cc2dac68e91bb2022fdb64a4f
Amalkrishnaur117 <amalkrishnaur117@gmail.com>
2025-08-26 11:55:18 +0530

    dded the stm32 project wit assembly and c
    `

---
+++ stm32f072_test/main.c
@@ -0,0 +1,14 @@
+
+#include <stdint.h>
+void _init(void) {}
+void _fini(void) {}
+volatile uint32_t result;
+
+int main(void) {
+    uint32_t a = 21;
+    uint32_t b = 21;
+    result = a + b;  // result = 42
+    while (1) {
+        // infinite loop so debugger can attach
+    }
+}
